715a5806f6091084750c1e030e3715b573e9375a
mako4kamiya <mako4kamiya@gmail.com>
2021-10-25 14:58:58 +0900

    pageごとにタイトルの切り替え

--- pages/main-header.php
+++ pages/main-header.php
@@ -1,12 +1,19 @@
 <?php
     session_start();
+    if (preg_match('/mypage/', $_SERVER['REQUEST_URI'])) {
+        $title = 'My page';
+    } elseif (preg_match('/score/', $_SERVER['REQUEST_URI'])) {
+        $title = 'SCORE';
+    } elseif (preg_match('/delete/', $_SERVER['REQUEST_URI'])) {
+        $title = 'EDIT';
+    }
 ?>
 
 <div class="main-header">
-    <h1 class="link-3">たいとる</h1>
+    <h1 class="link-3"><?php print($title) ?></h1>
     <div class="user-info">
-    <p><?php print($_SESSION['user']['studentNumber']); ?></p>
-    <p><?php print($_SESSION['user']['userName']); ?></p>
+        <p><?php print($_SESSION['user']['userName']); ?></p>
+        <p><?php print($_SESSION['user']['studentNumber']); ?></p>
     <p>******</p>
     </div>
 </div>
