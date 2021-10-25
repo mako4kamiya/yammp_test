3f2c6e4926c4aa72f1f6182f57a09cb7c6306f12
mako4kamiya <mako4kamiya@gmail.com>
2021-10-25 17:39:10 +0900

    ファビコンの追加

--- pages/html_head.php
+++ pages/html_head.php
@@ -26,6 +26,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="./css/style.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-
+  <link rel="icon" href="../favicon.ico" type="image/ico">
   <title><?php print($html_head_title); ?></title>
 </head>
