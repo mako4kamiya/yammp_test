967420ad80cc0a9e72b43b58ff11a9c5e037b0f0
mako4kamiya <mako4kamiya@gmail.com>
2021-10-26 16:11:16 +0900

    DB一旦シンプルに

--- db/migrations/20211026000002_add_questions_bank_table.php
+++ db/migrations/20211026000002_add_questions_bank_table.php
@@ -7,14 +7,13 @@ class AddQuestionsBankTable extends AbstractMigration
     public function up()
     {
         $sql = "CREATE TABLE `questionsBank` (
-            `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-            `field_id` int NOT NULL,
-            `toi` int NOT NULL,
-            `setsumon` varchar(2) NOT NULL,
-            `allotion` double NOT NULL DEFAULT 0,
+            `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
             `collectAnswer` varchar(1) NOT NULL,
+            `toi` INT NOT NULL,
+            `setsumon` varchar(2) NOT NULL,
+            `allotion` double NOT NULL,
+            `fieldName` TEXT NOT NULL,
             `examName` varchar(11) NOT NULL,
-            FOREIGN KEY fk_field_id(field_id) REFERENCES fieldsBank(id)
             );"
         ;
         $questions = $this->execute($sql);
--- db/migrations/20211026000004_add_user_answers_table.php
+++ db/migrations/20211026000004_add_user_answers_table.php
@@ -8,11 +8,11 @@ class AddUserAnswersTable extends AbstractMigration
     {
         $sql = "CREATE TABLE `user_answers`(
             `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-            `userID` INT(11) NOT NULL,
-            `questionID` INT(11) NOT NULL,
             `userAnswer` VARCHAR(1) NOT NULL,
             `selected` TINYINT(1) NOT NULL DEFAULT 0,
             `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+            `userID` INT(11) NOT NULL,
+            `questionID` INT(11) NOT NULL,
             FOREIGN KEY fk_userID(userID) REFERENCES users(id),
             FOREIGN KEY fk_questionID(questionID) REFERENCES questionsbank(id)
             );"
