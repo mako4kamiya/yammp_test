22f64603ee9caa970689739e56ff8fcb0e775b63
Mi-git34 <aydx4thlllpfz1z1i961srfnnkv@gmail.com>
2021-12-01 13:53:41 +0900

    いったん保存

--- pages/cbt_test4.php
+++ pages/cbt_test4.php
@@ -74,50 +74,87 @@
 <p id="timer" class="time-icon"></p>
 
 <script>
-let countdown = function(due){
-// 現在日時を取得
-let now = new Date();
-// 現在日時と引数dueの時間の差を取得
-let rest = due.getTime() - now.getTime();
-// 差の秒の値を取得
-let sec = Math.floor(rest / 1000) % 60;
-// 差の分の値を取得
-let min = Math.floor(rest / 1000 / 60) % 60;
-// 差の時間を取得
-let hours = Math.floor(rest / 1000 / 60 / 60) % 24;
-// 差の日付を取得
-let days = Math.floor(rest / 1000 / 60 / 60 /24);
-// countに差の一覧を代入
-let count = [days, hours, min, sec];
-// countを戻す
-return count;
-}
-// 期日を設定
-let goal = new Date();
-goal.setHours(13);
-goal.setMinutes(68);
-goal.setSeconds(68);
+//001----------javascriptの開始--------------------------------------------------
+    window.onload = function(){
+//002----------スタートボタンをクリックして発火--------------------------------------
+            var start = document.getElementById('start');
+            start.addEventListener('click',count_start, false);
+//003----------ストップボタンをクリックして発火--------------------------------------
+            var stop = document.getElementById("stop");
+            stop.addEventListener("click",count_stop, false);
+//004----------リセットボタンをクリックして発火--------------------------------------
+            var reset = document.getElementById("reset");
+            reset.addEventListener("click",count_reset,false);
+        }     
+//-----------------------------------------------------------------------------
+//変数の定義---------------------------------------------------------------------
+    var count   = 3600;     //カウントダウンの数字を格納する変数  3分なので180
+    var hour    = 0;
+    var min     = 0;       //残り時間「分」を格納する変数
+    var sec     = 0;       //残り時間「秒」を格納する変数
+    var start_f = false; 
+    var interval;
+//-----------------------------------------------------------------------------
 
-// recalcの中にファンクションを変数として代入
-let recalc = function(){
-// counterにcountdown(goal)の結果を代入
-let counter = countdown(goal);
-// counterの値をhtmlのIDがtimerの場所へ表示
-let time ="残り時間:\n" + counter[1] + ":" + counter[2] + ":" + counter[3];
-document.getElementById("timer").textContent = time;
-document.getElementById("timer").style.color = "white";
-// refreshを実行
-refresh();
-}
-let refresh = function(){
-// 指定した(1/1000 秒)時間の後にrecalcを実行
-setTimeout(recalc, 1000);
-}
-// recalc()を呼び出し
-recalc();
+//004----------スタート開始------------------------------------------------------
+function count_start(){
+//0041------------二度押しを防止する
+        if (start_f === false) {
+//0042------------1秒毎にcont_down関数を呼び出す
+          interval = setInterval(count_down,1000);
+          start_f = true;
+        }
+    }
+//-----------------------------------------------------------------------------
 
+//005------------カウントダウンの開始---------------------------------------------
+    function count_down(){
+//006--------------------------------------------------------------------------
+        if(count === 1){
+            var display = document.getElementById("default");
+            display.style.color = 'red';
+            display.innerHTML = "TIME UP!";
+            clearInterval(interval);
+//007-------------------------------------------------------------------------
+        } else {
+//008-------------------------------------------------------------------------
+            count--;
+//009----------Math.floor関数を使って小数点になった分を整数に変換する。---------------
 
-  </script>
+            hour = Math.floor(count / 1800);
+            min = Math.floor(count / 1800);
+//010------------60秒で割ったあまりが秒となる-------------------------------------
+            sec = count % 60;
+            var count_down = document.getElementById("default");
+            count_down.innerHTML = ("0"+hour) + "：" + ("0"+min) +"：" + ("0"+sec).slice(-2);
+        }
+    }
+
+//006-----------ストップボタンの押下---------------------------------------
+    function count_stop(){
+        clearInterval(interval);
+        start_f = false;
+    }
+
+//007-----------リセットボタンの押下---------------------------------------
+    function count_reset(){
+       clearInterval(interval);
+        count = 3600;
+        start_f = false;
+        var count_down = document.getElementById("default");
+        count_down.style.color = 'white';
+        count_down.innerHTML = "02：00：00";
+    }    
+    </script>
+
+    <!-----タイマーの3分を表示------------------------------------------>
+    <p id="default">02：00：00</p>
+<!-----スタートボタンを表示----------------------------------------->
+            <button id="start">スタート</button>
+<!-----ストップボタンを表示----------------------------------------->
+            <button id="stop">ストップ</button>
+<!-----リセットボタンを表示----------------------------------------->
+            <button id="reset">リセット</button>
 
 
         
@@ -134,6 +171,12 @@ recalc();
         <?php include("select-main.php"); ?>
       </div>
       <div class="select-footer">
+
+
+
+
+
+
       <button type="button" class="btn btn-primary btnsize">開始<span class="icon-click"></span></button>
       </div>
     </div>
