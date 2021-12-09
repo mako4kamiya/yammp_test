c9a0e879dc362fa74df1f1db61ef3904cdf76c52
mako4kamiya <mako4kamiya@gmail.com>
2021-12-09 12:00:59 +0900

    選択問題の表示

--- mypage_fecbt_pm_exam_copy_copy.php
+++ mypage_fecbt_pm_exam_copy_copy.php
@@ -47,41 +47,22 @@
     
     $sentaku_kigou = array("ア", "イ", "ウ", "エ", "オ", "カ", "キ", "ク", "ケ", "コ",);
 
-    // $sentakuGroup = $db->prepare('SELECT * FROM questions WHERE sentakuGroup = ? GROUP BY toi');
-    // $sentakuGroup = $db->prepare('SELECT toi FROM questions WHERE sentakuGroup = ? GROUP BY toi');
-
-    // $sentaku_1_first = array_key_first($sentaku_1);
-    // $sentaku_1_last = array_key_last($sentaku_1);
-    
-
-    // $hissyu_1 = $sentakuGroup->execute(['必修1']);
-    // $sentaku_1 = $sentakuGroup->execute(['選択1']);
-    // $hissyu_2 = $sentakuGroup->execute(['必修2']);
-    // $sentaku_2 = $sentakuGroup->execute(['選択2']);
-
-    // $mondaisu = $this_exam->rowCount();
-    
-
-    // $questions = $db->prepare('SELECT * FROM questions WHERE examName = ? AND toi = ?');
-
-    // $fieldNames = $db->prepare('SELECT fieldName FROM questions WHERE examName = "令和元年度秋期" AND toi = ?');
-
     if (!empty($_POST)) {
         // 選択した問題数
-        $sentakuGroup1; $sentakuGroup2;
-        for ($i = 0; $i < count($_POST['selected']); $i++) { 
-            if (key($_POST['selected'][$i]) == '選択1') {
-                $sentakuGroup1++;
-            } elseif (key($_POST['selected'][$i]) == '選択2') {
-                $sentakuGroup2++;
-            }
-        }
-        if ($sentakuGroup1 < 4 || $sentakuGroup2 < 1) {
-            $error['selected'] = 'not_enough';
-        } else {
-            $error['selected'] = 'ok' ;
-        }
-        $_SESSION['answers'] = $_POST;
+        // $sentakuGroup1; $sentakuGroup2;
+        // for ($i = 0; $i < count($_POST['selected']); $i++) { 
+        //     if (key($_POST['selected'][$i]) == '選択1') {
+        //         $sentakuGroup1++;
+        //     } elseif (key($_POST['selected'][$i]) == '選択2') {
+        //         $sentakuGroup2++;
+        //     }
+        // }
+        // if ($sentakuGroup1 < 4 || $sentakuGroup2 < 1) {
+        //     $error['selected'] = 'not_enough';
+        // } else {
+        //     $error['selected'] = 'ok' ;
+        // }
+        // $_SESSION['answers'] = $_POST;
     }
 
     // 書き直し
@@ -138,11 +119,9 @@
                     <!-- info内容 -->
                     <div class="tab-pane fade show active" id="info"  role="tabpanel">
                         <p>ここに説明</p>
-                        <input id="aiue" type="radio" name="aiue" >aaa
-                        <input id="aiue" type="radio" name="aiue" >iii
-                        <input id="aiue" type="radio" name="aiue" >uuu
-                        <input id="aiue" type="radio" name="aiue" >eee
-                        <label for="aiue">aiue</label>
+                        <?php
+                            var_export($_POST);
+                        ?>
                     </div>
                     <!-- 問題内容 -->
                     <?php $groupBySentakuGroup->execute([]) ?>
@@ -167,6 +146,7 @@
                             <?php elseif(preg_match("/選択/", $SentakuGroup['sentakuGroup'])): ?>
                                 <div class="nav flex-column nav-pills" role="tablist">
                                     <?php $whereSentakuGroupGroupByToi->execute([$SentakuGroup['sentakuGroup']]) ?>
+                                    <?php $i = true ?>
                                     <?php while($row = $whereSentakuGroupGroupByToi->fetch(PDO::FETCH_ASSOC)): ?>
                                         <?php if($i == true): ?>
                                             <a class="nav-link active" data-bs-toggle="pill" href="#<?php printf('%s-%s', $row['sentakuGroup'], $row['toi']) ?>" role="tab"><?php print $row['toi'] ?></a>
@@ -178,27 +158,52 @@
                                 </div>
                                 <div class="tab-content">
                                     <?php $whereSentakuGroupGroupByToi->execute([$SentakuGroup['sentakuGroup']]) ?>
+                                    <?php $j = true ?>
                                     <?php while($row = $whereSentakuGroupGroupByToi->fetch(PDO::FETCH_ASSOC)): ?>
-                                        <div class="tab-pane fade" id="<?php printf('%s-%s', $row['sentakuGroup'], $row['toi']) ?>" role="tabpanel">
-                                            <div>
-                                                <input hidden cheked name="<?php printf('id_%s[][selected]', $row['id']) ?>" value="0" class="form-check-input" type="radio">
-                                                <input  id="<?php printf('toi_%s-selected', $row['toi']) ?>" name="<?php printf('id_%s[][selected]', $row['id']) ?>" value="1" class="form-check-input" type="radio">
-                                                <label for="<?php printf('toi_%s-selected', $row['toi']) ?>" class="form-check-label">この問題を選択する</label>
-                                            </div>
-                                            <?php $whereSentakuGroupToi->execute([$row['sentakuGroup'], $row['toi']]) ?>
-                                            <?php while($row = $whereSentakuGroupToi->fetch()): ?>
-                                                <p>設問<?php print $row['setsumon'] ?>に関する解答群</p>
+                                        <?php if($j == true): ?>
+                                            <div class="tab-pane fade show active" id="<?php printf('%s-%s', $row['sentakuGroup'], $row['toi']) ?>" role="tabpanel">
                                                 <div>
-                                                    <input hidden id="<?php printf('toi_%s-selected', $row['toi']) ?>" name="<?php printf('id_%s[][selected]', $row['id']) ?>" value="1">
-                                                    <?php for($i = 0; $i < $row['sentakushi']; $i++): ?>
-                                                        <div>
-                                                            <input  id="<?php printf('id_%s-%s-%s', $row['id'], $row['setsumon'], $sentaku_kigou[$i]) ?>" name="<?php printf('id_%s[][userAnswer]', $row['id']) ?>" value="<?php print $sentaku_kigou[$i] ?>" type="radio" class="btn-check">
-                                                            <label for="<?php printf('id_%s-%s-%s', $row['id'], $row['setsumon'], $sentaku_kigou[$i]) ?>" class="btn btn-outline-dark"><?php print $sentaku_kigou[$i] ?></label>
-                                                        </div>
-                                                    <?php endfor; ?>
+                                                    <input hidden cheked name="<?php printf('id_%s[][selected]', $row['id']) ?>" value="0" class="form-check-input" type="radio">
+                                                    <input  id="<?php printf('toi_%s-selected', $row['toi']) ?>" name="<?php printf('id_%s[][selected]', $row['id']) ?>" value="1" class="form-check-input" type="radio">
+                                                    <label for="<?php printf('toi_%s-selected', $row['toi']) ?>" class="form-check-label">この問題を選択する</label>
                                                 </div>
-                                            <?php endwhile; ?>
-                                        </div>
+                                                <?php $whereSentakuGroupToi->execute([$row['sentakuGroup'], $row['toi']]) ?>
+                                                <?php while($row = $whereSentakuGroupToi->fetch()): ?>
+                                                    <p>設問<?php print $row['setsumon'] ?>に関する解答群</p>
+                                                    <div>
+                                                        <input hidden id="<?php printf('toi_%s-selected', $row['toi']) ?>" name="<?php printf('id_%s[][selected]', $row['id']) ?>" value="1">
+                                                        <?php for($i = 0; $i < $row['sentakushi']; $i++): ?>
+                                                            <div>
+                                                                <input  id="<?php printf('id_%s-%s-%s', $row['id'], $row['setsumon'], $sentaku_kigou[$i]) ?>" name="<?php printf('id_%s[][userAnswer]', $row['id']) ?>" value="<?php print $sentaku_kigou[$i] ?>" type="radio" class="btn-check">
+                                                                <label for="<?php printf('id_%s-%s-%s', $row['id'], $row['setsumon'], $sentaku_kigou[$i]) ?>" class="btn btn-outline-dark"><?php print $sentaku_kigou[$i] ?></label>
+                                                            </div>
+                                                        <?php endfor; ?>
+                                                    </div>
+                                                <?php endwhile; ?>
+                                            </div>
+                                            <?php $j = false; ?>
+                                        <?php else: ?>
+                                            <div class="tab-pane fade" id="<?php printf('%s-%s', $row['sentakuGroup'], $row['toi']) ?>" role="tabpanel">
+                                                <div>
+                                                    <input hidden cheked name="<?php printf('id_%s[][selected]', $row['id']) ?>" value="0" class="form-check-input" type="radio">
+                                                    <input  id="<?php printf('toi_%s-selected', $row['toi']) ?>" name="<?php printf('id_%s[][selected]', $row['id']) ?>" value="1" class="form-check-input" type="radio">
+                                                    <label for="<?php printf('toi_%s-selected', $row['toi']) ?>" class="form-check-label">この問題を選択する</label>
+                                                </div>
+                                                <?php $whereSentakuGroupToi->execute([$row['sentakuGroup'], $row['toi']]) ?>
+                                                <?php while($row = $whereSentakuGroupToi->fetch()): ?>
+                                                    <p>設問<?php print $row['setsumon'] ?>に関する解答群</p>
+                                                    <div>
+                                                        <input hidden id="<?php printf('toi_%s-selected', $row['toi']) ?>" name="<?php printf('id_%s[][selected]', $row['id']) ?>" value="1">
+                                                        <?php for($i = 0; $i < $row['sentakushi']; $i++): ?>
+                                                            <div>
+                                                                <input  id="<?php printf('id_%s-%s-%s', $row['id'], $row['setsumon'], $sentaku_kigou[$i]) ?>" name="<?php printf('id_%s[][userAnswer]', $row['id']) ?>" value="<?php print $sentaku_kigou[$i] ?>" type="radio" class="btn-check">
+                                                                <label for="<?php printf('id_%s-%s-%s', $row['id'], $row['setsumon'], $sentaku_kigou[$i]) ?>" class="btn btn-outline-dark"><?php print $sentaku_kigou[$i] ?></label>
+                                                            </div>
+                                                        <?php endfor; ?>
+                                                    </div>
+                                                <?php endwhile; ?>
+                                            </div>
+                                        <?php endif; ?>
                                     <?php endwhile; ?>
                                 </div>
                             <?php endif ?>
